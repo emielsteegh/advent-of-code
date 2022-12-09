659645bf97596f6e76cf4a7829867733da98ee22
emiel <emielsteegh@gmail.com>
2022-12-09 20:11:09 +0100

    06b

--- 06_script.py
+++ 06_script.py
@@ -1,8 +1,18 @@
 
-
 INPUT_FILE = "06_input.txt"
+MARKER_LEN = 14
 if __name__ == "__main__":
     
     with open(INPUT_FILE) as file:
         for line in file:
             line=line[:-1]
+            buffer = line[:MARKER_LEN-1]
+            location = 1
+            for ch in range(MARKER_LEN-1,len(line)+1):
+                # print(set(buffer+line[ch]))
+                if len(set(buffer+line[ch])) == MARKER_LEN:
+                    print(ch+1)
+                    break
+                else:
+                    buffer = buffer[1:] + line[ch]
+                
