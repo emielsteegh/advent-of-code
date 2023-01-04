292eaa9466630b6ec13aa6e8ebf15c1907df7719
emiel <emielsteegh@gmail.com>
2023-01-04 22:29:45 +0100

    aoc module cleanup and timed decorator

--- aoc/getters.py
+++ aoc/getters.py
@@ -1,18 +1,18 @@
+from .helpers import string_to_numbers
 from os.path import join, basename
 import inspect
-import re
 
 NOWPY = "now.py"
 NOWIN = "now.txt"
 
 
 def lines(as_list=True):
-    '''
+    """
     based on the path of the calling script (not this one),
     returns the input of the matching advent of code day as lines
     if called by now.py returns the content of now.txt
     if called by an archived date.py will return the content of that date.txt
-    '''
+    """
     fname = None
     caller = inspect.stack()  # [1].filename
     caller = [stack.filename for stack in caller]
@@ -23,7 +23,7 @@ def lines(as_list=True):
     else:
         # the idea is that from the path we get the in txt
         caller = caller.split("/")
-        fname = join(caller[-2], "in", caller[-1][:-2]+"txt")
+        fname = join(caller[-2], "in", caller[-1][:-2] + "txt")
         pass
     f = open(fname, "r")
     if as_list:
@@ -34,26 +34,15 @@ def lines(as_list=True):
     return lines
 
 
-def string_to_numbers(s: str, delimiter: str = ','):
-    # start with or without -, <0 digits, with or without a single delmiter character followed by <0 digits
-    if delimiter not in ['.', ',', None]:
-        raise Exception("Delimiter must be ',' or '.' or None")
-
-    re_numbers = re.compile(
-        f'(-?\d+(?:\{delimiter}\d+)?)' if delimiter != None else f'(-?\d+)')
-
-    def convert(x): return float(x.replace(",", ".")
-                                 ) if delimiter != None else int(x)  # ! not very effcient
-    numbers_in_string = [(convert(x)) for x in re.findall(re_numbers, s)]
-    return numbers_in_string
-
-
-def numbers(delimiter: str = ',', keep_empty=True):
-    '''takes the appropriate input to a script,
-     uses simple regex to convert them to a list of numbers per line'''
+def numbers(delimiter: str = ",", keep_empty=True):
+    """takes the appropriate input to a script,
+    uses simple regex to convert them to a list of numbers per line"""
 
     ls = lines()
     # returns all lines as a list of the numbers in it
-    numbers = [string_to_numbers(s=line, delimiter=delimiter)
-               for line in ls if (keep_empty or line != '')]
+    numbers = [
+        string_to_numbers(s=line, delimiter=delimiter)
+        for line in ls
+        if (keep_empty or line != "")
+    ]
     return numbers
--- aoc/helpers.py
+++ aoc/helpers.py
@@ -1,29 +1,65 @@
 from os.path import join, exists
 from os import remove
 from sys import exit
+from functools import wraps
+import re
+import time
 
 
-def if_exists(day, year, force:bool=False):
+def if_exists(day, year, force: bool = False):
     scr, inp = get_fnames(day, year)
     scr_exist = exists(scr)
     inp_exist = exists(inp)
     if not force and (scr_exist or inp_exist):
-        exit(f"""
+        exit(
+            f"""
         Error! Stopped archiving operation!
         {scr} {"already exists" if scr_exist else "does not yet exist"}
         {inp} {"already exists" if inp_exist else "does not yet exist"}
-        pass -F to force overwrite or remove them manually\n""")
+        pass -F to force overwrite or remove them manually\n"""
+        )
     elif force:
-        if scr_exist: 
+        if scr_exist:
             remove(scr)
             print(f"Removed {scr}")
-        if inp_exist: 
+        if inp_exist:
             remove(inp)
             print(f"Removed {inp}")
 
+
 def get_fnames(day, year):
     """returns filenames of script and input for day, year"""
-    script_fname  = join(str(year), f"{day}.py")
+    script_fname = join(str(year), f"{day}.py")
     input_fname = join(str(year), "in", f"{day}.txt")
 
     return script_fname, input_fname
+
+
+def string_to_numbers(s: str, delimiter: str = ","):
+    # start with or without -, <0 digits, with or without a single delmiter character followed by <0 digits
+    if delimiter not in [".", ",", None]:
+        raise Exception("Delimiter must be ',' or '.' or None")
+
+    re_numbers = re.compile(
+        f"(-?\d+(?:\{delimiter}\d+)?)" if delimiter != None else f"(-?\d+)"
+    )
+    convert = (
+        lambda x: float(x.replace(",", ".")) if delimiter != None else int(x)
+    )  # ! not very effcient
+    numbers_in_string = [(convert(x)) for x in re.findall(re_numbers, s)]
+    return numbers_in_string
+
+
+def timer(func):
+    @wraps(func)
+    def timer_wrapper(*args, **kwargs):
+        pre = time.perf_counter()
+        result = func(*args, **kwargs)
+        post = time.perf_counter()
+        elapsed_time = post - pre
+        print(
+            f"-> {func.__name__}({args}, {kwargs}) ran in: {elapsed_time:0.5f} seconds"
+        )
+        return result
+
+    return timer_wrapper
